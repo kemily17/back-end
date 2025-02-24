5cd0991880d12ace96556417544f897cfb19c2a0
kemily <kemilymichaele@gmail.com>
2025-02-24 11:55:31 -0300

    first commit

---
+++ aula1.js
@@ -0,0 +1,13 @@
+ // calciladora em javaScript
+ const valor1 = 1;
+ const valor2 = 2;
+ let soma = valor1 + valor2;
+ let sub = valor1 - valor2;
+ let mult = valor1 * valor2;
+ let div = valor1 / valor2; 
+ 
+ console.log("A soma é: " + soma);
+ console.log("A subtração é: " + sub);
+ console.log("A multiplição é: " + mult);
+ console.log("A divisão é: " + div);
+ // node nome-arquivo.js
---
+++ boolean.js
@@ -0,0 +1,2 @@
+//boolean.js
+let estudantes = 
