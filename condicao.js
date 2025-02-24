f03fe8ea751693991ca3c0a19e055538a55305cd
kemily17 <164501395+kemily17@users.noreply.github.com>
2025-02-24 12:02:46 -0300

    Update condicao.js

---
+++ condicao.js
@@ -0,0 +1,14 @@
+// condição
+/* se (condição){
+    codigo aqui
+} se não {
+    codigo aqui }
+*/
+let idade = 18
+let possuiCNH = true
+if(idade >= 18 && possuiCNH == true){
+    console.log("voce pode dirigir!")   
+  } else {
+     console.log("ande de onibus ou uber!")
+}
+ 
