007fe0df53be7437a7f089a2b88805d8e38ced50
kemily17 <164501395+kemily17@users.noreply.github.com>
2025-02-24 12:03:12 -0300

    Update exercicio1.js

---
+++ exercicio1.js
@@ -0,0 +1,10 @@
+let av1 = 5.0;
+let av2 = 6.0;
+let recup1 = 7.0;
+let recup2 = 3.0;
+/* A escola adota uma recuperação para cada avaliação,
+que substitui a nota do aluno caso seja maior que
+sua nota anterior.
+Dada as seguintes notas, calcule a média simples
+do aluno e diga se ele está aprovado ou reprovado,
+levando em consideração a substituição de notas. */
